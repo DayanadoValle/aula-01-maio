074049c4c0e9642331813a005902ac95e0f49863
dayanadovalle <isaque.dayana@gmail.com>
2023-05-10 17:46:20 -0300

    exercicio prompt

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('oi');
+console.log('oi, tudo bem?');
+
+//PROMPT
+const qualSeuNome = prompt ('Qual o seu nome?');
+
+console.log(qualSeuNome);
+
+const qualSuaIdade = prompt ('Qual a sua idade?');
+
+console.log(qualSuaIdade);
