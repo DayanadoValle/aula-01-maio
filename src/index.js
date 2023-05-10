2221e421cad8b9b2dd406bd2cf0f0629e23b08f6
dayanadovalle <isaque.dayana@gmail.com>
2023-05-10 18:00:51 -0300

    exercicio prompt e alert

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ console.log('oi, tudo bem?');
 //PROMPT
 const qualSeuNome = prompt ('Qual o seu nome?');
 
-console.log(qualSeuNome);
+alert("Seu nome é " + qualSeuNome);
 
 const qualSuaIdade = prompt ('Qual a sua idade?');
 
-console.log(qualSuaIdade);
+alert("Sua idade é " + qualSuaIdade);
